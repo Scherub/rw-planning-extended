99e67e44eb528146a4d201d06dacf19ecdd57a97
Scherub <steffen.engel@zfx.info>
2022-10-23 19:10:21 +0200

    Replaced old icons

--- Source/PlanningExtended 1.4/Source/Definitions/Textures.cs
+++ Source/PlanningExtended 1.4/Source/Definitions/Textures.cs
@@ -7,5 +7,13 @@ namespace PlanningExtended
     public static class Textures
     {
         public static readonly Texture2D ShowPlanToggleIcon = ContentFinder<Texture2D>.Get("UI/ShowPlanToggle", true);
+        
+        public static readonly Texture2D RotateLeft = ContentFinder<Texture2D>.Get("UI/RotateLeft", true);
+        
+        public static readonly Texture2D RotateRight = ContentFinder<Texture2D>.Get("UI/RotateRight", true);
+        
+        public static readonly Texture2D FlipHorizontal = ContentFinder<Texture2D>.Get("UI/FlipHorizontal", true);
+        
+        public static readonly Texture2D FlipVertical = ContentFinder<Texture2D>.Get("UI/FlipVertical", true);
     }
 }
