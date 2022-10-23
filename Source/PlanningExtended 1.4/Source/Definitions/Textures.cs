bfd7c49f32e8679d7e2ae050ea81faeaa11c0ceb
Scherub <steffen.engel@zfx.info>
2022-10-23 16:39:24 +0200

    Added visibility toggle for plans

---
+++ Source/PlanningExtended 1.4/Source/Definitions/Textures.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace PlanningExtended
+{
+    [StaticConstructorOnStartup]
+    public static class Textures
+    {
+        public static readonly Texture2D ShowPlanToggleIcon = ContentFinder<Texture2D>.Get("UI/ShowPlanToggle", true);
+    }
+}
---
+++ Source/PlanningExtended 1.4/Source/Patches/PatchPlaySettings.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using PlanningExtended.Plans;
+using RimWorld;
+using Verse;
+
+namespace PlanningExtended.Patches
+{
+    [HarmonyPatch(typeof(PlaySettings), "DoPlaySettingsGlobalControls")]
+    class PatchPlaySettings
+    {
+        static void Postfix(WidgetRow row, bool worldView)
+        {
+            if (worldView || row == null)
+                return;
+
+            bool arePlansVisible = PlanManager.ArePlansVisible;
+
+            row.ToggleableIcon(ref arePlansVisible, Textures.ShowPlanToggleIcon, "PlanningExtended.Settings.ArePlansVisible.Label".Translate(), SoundDefOf.Mouseover_ButtonToggle);
+
+            PlanManager.SetArePlansVisible(arePlansVisible);
+        }
+    }
+}
