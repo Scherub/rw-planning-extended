e64a39f938fc6478fd6229740daed890a73b89ef
Scherub <steffen.engel@zfx.info>
2022-10-23 16:38:00 +0200

    Fixed 'Cancel' designator removes plan designations

---
+++ Source/PlanningExtended 1.4/Source/Patches/PatchDesignatorCancel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using PlanningExtended.Designations;
+using RimWorld;
+using Verse;
+
+namespace PlanningExtended.Patches
+{
+    [HarmonyPatch(typeof(Designator_Cancel), "CancelableDesignationsAt")]
+    class PatchDesignatorCancel
+    {
+        public static IEnumerable<Designation> Postfix(IEnumerable<Designation> result)
+        {
+            return result.Where(d => d is not PlanDesignation);
+        }
+    }
+}
