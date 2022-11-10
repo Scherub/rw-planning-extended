7e184c9fc9d58363780b3ded79441a00ab9388ee
Scherub <steffen.engel@zfx.info>
2022-11-10 11:54:49 +0100

    Added sound on button click

--- Source/PlanningExtended 1.4/Source/Designators/Other/TogglePlanVisibilityDesignator.cs
+++ Source/PlanningExtended 1.4/Source/Designators/Other/TogglePlanVisibilityDesignator.cs
@@ -1,5 +1,7 @@
 ﻿using PlanningExtended.Plans;
+using RimWorld;
 using UnityEngine;
+using Verse.Sound;
 
 namespace PlanningExtended.Designators
 {
@@ -15,6 +17,12 @@ namespace PlanningExtended.Designators
         public override void ProcessInput(Event ev)
         {
             PlanManager.SetArePlansVisible(!PlanManager.ArePlansVisible);
+
+            if (PlanManager.ArePlansVisible)
+                SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera(null);
+            else
+                SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera(null);
+
         }
     }
 }
