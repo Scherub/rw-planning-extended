c104e66d9386a6027dbbc80a43f7971cc71ba2cc
Scherub <steffen.engel@zfx.info>
2023-05-16 16:12:40 +0200

    Added shape content modifiers (idea)

---
+++ Source/PlanningExtended 1.4/Source/Shapes/Modifiers/Content/BaseShapeContentModifier.cs
@@ -0,0 +1,10 @@
+﻿using PlanningExtended.Cells;
+using Verse;
+
+namespace PlanningExtended.Shapes.Modifiers.Content
+{
+    public abstract class BaseShapeContentModifier
+    {
+        public abstract AreaDimensions Update(BaseShape shape, AreaDimensions areaDimensions, IntVec3 mousePosition, Direction rotation);
+    }
+}
